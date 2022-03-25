b6d2665f45da90ffc842fd5561712e5534c77fd6
gregor <wpumacay@gmail.com>
2022-03-25 16:49:27 -0500

    [fix]: adds __cplusplus flag for MSVC

--- cmake/SetupTarget.cmake
+++ cmake/SetupTarget.cmake
@@ -53,6 +53,7 @@ set(MSVC_BASE_WARNINGS
     /w14906 # string literal cast to 'LPWSTR'
     /w14928 # illegal copy-initialization; more than one user-defined conversion has been implicitly applied
     /permissive- # standards conformance mode for MSVC compiler.
+    /Zc:__cplusplus # expose the __cplusplus version
 )
 
 # -------------------------------------
